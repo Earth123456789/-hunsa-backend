dbf5747d498084ac5ddf63cf8445d334e90609ea
Earth x น้องพายุ <109953632+Earth123456789@users.noreply.github.com>
2024-11-04 23:05:24 +0700

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
-import express, { Application } from 'express';
-import cors from 'cors';
-import EmployeeRoutes from './routes/employeeRoutes';
-import ServiceRoutes from './routes/serviceRoutes';
-import ReservationRoutes from './routes/reservationRoutes';
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+import reservationRoutes from "./routes/reservationRoutes";
+import employeeRoutes from "./routes/employeeRoutes";
+import serviceRoutes from "./routes/serviceRoutes";
 
 const app: Application = express();
 
@@ -11,13 +11,19 @@ app.use(express.json()); // รับส่งข้อมูลแบบ JSON
 app.use(cors()); // frontend เรียกใช้ API ได้
 
 // Base Routes
-app.use('/api/employees', EmployeeRoutes);
-app.use('/api/services', ServiceRoutes);
-app.use('/api/reservation', ReservationRoutes);
+app.use("/api/employees", employeeRoutes);
+app.use("/api/reservation", reservationRoutes);
+app.use("/api/services", serviceRoutes);
 
-// TEST API
-app.get('/api/', (req, res) => {
-    res.send('Welcome to API BACKEND');
-});
+app.get("/api/test", async (req, res) => {
+    try {
+      // Sample query to MongoDB or any test logic here
+      res.status(200).json({ message: "API is working" });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Server Error" });
+    }
+  });
+  
 
 export default app;
